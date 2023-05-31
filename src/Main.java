d09ae58cd5e25133bb83fb7e5766e75d1c0d66a1
Rasim <rasimzalyalov05@gmail.com>
2023-05-31 15:42:32 +0300

    Third commit

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,9 @@ public class Main {
         testing.sumTest();
         System.out.println("--- Update value test ---");
         testing.updateTest();
+        System.out.println("--- Average time of sum query test ---");
+        testing.averageTimeSumTest();
+        System.out.println("--- Average time of update value test ---");
+        testing.averageTimeUpdateTest();
     }
 }
--- src/SegmentTreeTesting.java
+++ src/SegmentTreeTesting.java
@@ -21,4 +21,32 @@ public class SegmentTreeTesting {
             System.out.printf("%d %d\n", arraySize, st.iterations);
         }
     }
+    public void averageTimeSumTest() {
+        int arraySize = 100000;
+        int[] array = new int[arraySize];
+        SegmentTree st = new SegmentTree(array);
+        for (int i = 1; i <= 50; i++) {
+            int l = (int) (Math.random() * arraySize / 2);
+            int r = (int) (Math.random() * arraySize / 2 + arraySize / 2);
+            long startTime = System.nanoTime();
+            st.sum(l, r);
+            long endTime = System.nanoTime();
+            System.out.printf("%d %d %d\n", i, endTime - startTime, st.iterations);
+            st.iterations = 0;
+        }
+    }
+    public void averageTimeUpdateTest() {
+        int arraySize = 100000;
+        int[] array = new int[arraySize];
+        SegmentTree st = new SegmentTree(array);
+        for (int i = 1; i <= 50; i++) {
+            int index = (int) (Math.random() * arraySize);
+            int newValue = (int) (Math.random() * 10000 + 1);
+            long startTime = System.nanoTime();
+            st.update(index, newValue);
+            long endTime = System.nanoTime();
+            System.out.printf("%d %d %d\n", i, endTime - startTime, st.iterations);
+            st.iterations = 0;
+        }
+    }
 }
